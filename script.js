bdbc9a23e0a981b66a05544c61bbd823a9dd6d98
Anthony Guerrero <adguerrero1935@gmail.com>
2020-02-05 16:04:37 -0500

    updated Jquery and html

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+$("#find-city").on("click", function(){
+    event.preventDefault();
+
+    var searchForCity = $("#find-city").val();
+    var queryURL = "api.openweathermap.org/data/2.5/weather?" + searchForCity + "apid=672a1f598eafe184b59f5edbe13124b3";
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }) .then(function(){
+        console.log(queryURL);
+        
+    })
+});
+
+
+
+
+    
+
+
