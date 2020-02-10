342ce8188bb0cccb308202cd466508550339b9db
Anthony Guerrero <adguerrero1935@gmail.com>
2020-02-09 22:46:31 -0500

    updated fiveDayForecast

--- script.js
+++ script.js
@@ -1,45 +1,84 @@
-var city =[];
+var city = [];
 
 
-$("#find-city").on("click", function(){
+$("#find-city").on("click", function (event) {
     event.preventDefault();
-    var searchForCity = $("#search-input").val();
-    var queryURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q="+ searchForCity +"&appid=672a1f598eafe184b59f5edbe13124b3";
-    var uvApi = "http://api.openweathermap.org/data/2.5/uvi?appid=672a1f598eafe184b59f5edbe13124b3&lat=&lon=";
+    var searchForCity = $("#search-input").val().trim();
+    if (!searchForCity) return;
+    $("#search-input").val("");
+    renderCity(searchForCity);
+    getCityWeather(searchForCity);
+
+    var fiveDayForecast = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&q=" + searchForCity + "&appid=672a1f598eafe184b59f5edbe13124b3";
 
     $.ajax({
-        url: queryURL, uvApi,
-        method: "GET"
-    }) .then(function(response){
-        console.log(response, uvApi);
+        url: fiveDayForecast,
+        method: "GET",
+    }).then(function (response) {
+
+        var fiveDays =response.list;
         
-        
+        for(var i=0; i <5; i++){
+            console.log(fiveDays[i]);
+            
+            var fiveDaysDiv = $("<div>");
+            var days = $("<h3>").text(fiveDays[i].dt_txt);
+            var icon = $("<p>").text(fiveDays[i].weather[0].description);
+            var fiveDaysTemp = $("<p>").html("<p>Temperature: " + fiveDays[i].main.temp + "</p>");
+            var fiveDaysHumidty = $("<p>").html("<p> Humidity: " + fiveDays[i].main.humidity + "%</p>");
+            
+            fiveDaysDiv.append(days,icon,fiveDaysTemp,fiveDaysHumidty);
+            $(".fiveDaysForecast").append(fiveDaysDiv);
+            fiveDaysDiv.append(days,icon,fiveDaysTemp,fiveDaysHumidty);
+            $(".fiveDaysForecast2").append(fiveDaysDiv);
+           
 
-        $(".temp").html("<p> Temperature: " + response.main.temp + "</p>");
-        $(".humidity").html("<p> Humidity: " +response.main.humidity + "%</p>");
-        $(".wind-speed").html("<p> Wind Speed: " + response.wind.speed + " MPH</p>");
-        //$(".uv-index").html("<p> Uv Index:" + response.)\
+        };
 
-    })
-
+    });
 
 
 });
 
-function renderCity(){
-    for (var i=0; i < city.length; i++){
 
-        var newDiv = $("<li>");
-        newDiv.addclass("list-group-item");
-        newDiv.attr("data-city",city[i]);
-        newDiv.text(city[i]);
-        $(".city-results").append(newDiv);
-  
+function renderCity(newcity) {
+    city.push(newcity);
+    $(".city-results").empty();
+    for (var i = 0; i < city.length; i++) {
+        var newLi = $("<button>");
+        newLi.addClass("list-group-item list-group-item-action city-li");
+        newLi.attr("data-city", city[i]);
+        newLi.text(city[i]);
+        $(".city-results").append(newLi);
     }
 
-
-
-
 }
- 
 
+$(document).on('click', '.city-li', function () {
+    var city = $(this).attr('data-city');
+    getCityWeather(city);
+});
+
+
+function getCityWeather(city) {
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?units=imperial&q=" + city + "&appid=672a1f598eafe184b59f5edbe13124b3";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        $(".temp").html("<p> Temperature: " + response.main.temp + "</p>");
+        $(".humidity").html("<p> Humidity: " + response.main.humidity + "%</p>");
+        $(".wind-speed").html("<p> Wind Speed: " + response.wind.speed + " MPH</p>");
+
+        var uvApiBase = "https://api.openweathermap.org/data/2.5/uvi?appid=672a1f598eafe184b59f5edbe13124b3";
+        uvApiBase += "&lat=" + response.coord.lat;
+        uvApiBase += "&lon=" + response.coord.lon;
+        $.ajax({
+            url: uvApiBase,
+            method: "GET",
+        }).then(function (uvresponse) {
+            $(".uv-index").html("<p> UV Index: " + uvresponse.value + "</p>");
+        });
+    });
+}
